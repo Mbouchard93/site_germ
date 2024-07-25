df60635cbeebb161188e9aedce1de585b54cc57c
Mbouchard93 <mariiane.bouchard12@gmail.com>
2024-07-25 14:34:07 -0400

    commit

--- src/assests/js/main.js
+++ src/assests/js/main.js
@@ -1,9 +1,8 @@
 import '../css/style.css'
-/*
+
 document.addEventListener('DOMContentLoaded', () => {
     const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
     if (isSafari) {
       document.querySelector('.background-container').classList.add('safari-bg-fixed');
     }
   });
-  */
